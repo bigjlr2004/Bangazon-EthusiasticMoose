27c38ce09c42aaf75513803004cba4356b5d72de
John Roberts <bigjlr2004@yahoo.com>
2023-08-16 11:04:25 -0500

    Finished

--- Program.cs
+++ Program.cs
@@ -1,12 +1,146 @@
 ﻿using System;
+Main();
 
-namespace EnthusiasticMoose
+void Main()
 {
-    class Program
+    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    // Let the moose speak!
+    MooseSays("H I, I'M  E N T H U S I A S T I C !");
+    MooseSays("I really am enthusiastic");
+
+    // Ask a question
+    genericQuestion("Are you enthusiastic?", "Yay!", "You should try it!");
+    genericQuestion("Do you love CSharp Yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will...!");
+    genericQuestion("Do you want to know a secret??", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!");
+    genericQuestion("Is Canada Real?", "Really? It seems very unlikely.", "I KNEW IT !!!");
+
+}
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
     }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |   {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+
+
+void CanadaQuestion()
+{
+    bool isTrue = MooseAsks("Is Canada Real?");
+    if(isTrue)
+    {
+        MooseSays("Really? Its seems very unlikely.");
+    }
+    else
+    {
+        MooseSays("I   K N E W   I T !!!");
+    }
+}
+void EnthusiasticQuestion()
+{
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+}
+
+void genericQuestion(string genericQuestion, string positiveResponse, string negativeResponse)
+{
+    MooseSays(genericQuestion);
+    Console.Write(" (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+    if (answer == "y")
+    {
+        MooseSays(positiveResponse);
+    }
+    else if (answer == "n")
+    {
+        MooseSays(negativeResponse);
+    }
+    else
+    {
+        MooseSays(genericQuestion);
+    }
+
 }
